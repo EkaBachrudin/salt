e4b3c9566760bf48ebce60c804cf6b33f15de717
EkaBachrudin <65301748+EkaBachrudin@users.noreply.github.com>
2023-02-01 16:11:43 +0700

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import 'zone.js';
+import 'zone.js/dist/long-stack-trace-zone.js';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SaltModule } from './salt/salt.module';
